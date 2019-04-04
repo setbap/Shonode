0384fa4c7382bea61ea2fdb11eb7c77a4cf886c0
sina <ebr.sina@outlook.com>
2019-04-04 05:18:46 +0430

    add basic models

---
+++ models/Product.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const productSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: String,
+			required: true,
+		},
+		offerPrice: {
+			type: String,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		imageUrl: {
+			type: String,
+			required: true,
+		},
+		creator: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		spec: [
+			{
+				name: { type: String, required: true },
+				desc: { type: String, required: true },
+			},
+		],
+		comments: [
+			{
+				title: { type: String, required: true, maxlength: 255 },
+				content: { type: String, required: true },
+				userId: {
+					type: Schema.Types.ObjectId,
+					ref: "User",
+					required: true,
+				},
+			},
+		],
+	},
+	{ timestamps: true },
+);
+productSchema.methods.addComment = function(content, userId) {
+	this.comments.push({
+		content,
+		userId,
+	});
+	return this.save();
+};
