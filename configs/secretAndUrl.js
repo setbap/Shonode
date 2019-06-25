2ddfaf282869143a2186e18dc60dd0984d29a087
sina <ebr.sina@outlook.com>
2019-06-25 07:21:13 +0430

    change db :)

--- configs/secretAndUrl.js
+++ configs/secretAndUrl.js
@@ -1,6 +1,6 @@
-module.exports.DBurl = "mongodb://localhost/shonod";
-// module.exports.DBurl =
-// 	"mongodb://sinaE:sina12345@ds034807.mlab.com:34807/shonode";
+// module.exports.DBurl = "mongodb://localhost/shonod";
+module.exports.DBurl =
+	"mongodb://sinaE:sina12345@ds034807.mlab.com:34807/shonode";
 module.exports.jwtSecret = "mongodb://localhost/shonod";
 exports.pass = "$2a$12$ngwUZb07S6oDWueVW27cpuD93b2Qr2Od2nlr.9ec3Qv5sp/lLxePa";
 exports.adminEmail = "ebrsina@gmail.com";
